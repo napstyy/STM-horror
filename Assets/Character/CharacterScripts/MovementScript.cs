70055ec50543a4aafef915c5da5ca4fcdb5e4bac
Domenico Fabre Barile <domenicobarille@gmail.com>
2026-03-11 16:11:42 -0400

    MovementScriptForAnimationsStarted
    Started Movement Script now animations for frog need to be completed

--- Assets/Character/CharacterScripts/MovementScript.cs
+++ Assets/Character/CharacterScripts/MovementScript.cs
@@ -7,23 +7,37 @@ public class MovementScript : MonoBehaviour
     public float speed;
     private Rigidbody2D _rb;
     private Vector2 moveInput;
+    private Animator _animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody2D> ();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         _rb.linearVelocity = moveInput * speed;
 
         
     }
 
-    public void move(InputAction.CallbackContext context)
+    public void Move(InputAction.CallbackContext context)
     {
+        _animator.SetBool("isWalking", true);
+        if (context.canceled)
+        {
+            _animator.SetBool("isWalking", false);
+            _animator.SetFloat("LastInputX", moveInput.x);
+            _animator.SetFloat("LastInputY", moveInput.y);
+        }
         moveInput = context.ReadValue<Vector2>();
+        _animator.SetFloat("InputX", moveInput.x);
+        _animator.SetFloat("InputY", moveInput.y);
+      
+        
     }
     
 }
