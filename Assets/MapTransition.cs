b149377c52abfbb272ec4de2f4b70a5c35470553
Domenico Fabre Barile <domenicobarille@gmail.com>
2026-03-12 23:39:06 -0400

    Added Transition points
    Looked up a script took me 4 hours

---
+++ Assets/MapTransition.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField]private PolygonCollider2D mapBoundary;
+    private CinemachineConfiner2D confiner;
+    [SerializeField]private Direction direction;
+    [SerializeField]private float additivePos = 2f;
+
+    private enum Direction {Up, Down, Left, Right}
+
+    private void Awake()
+    {
+        confiner = FindFirstObjectByType<CinemachineConfiner2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundary;
+            UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+    
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += 2;
+                break;
+
+            case Direction.Down:
+                newPos.y -= 2;
+                break;
+
+            case Direction.Left:
+                newPos.x -= 2;
+                break;
+
+            case Direction.Right:
+                newPos.x += 2;
+                break;
+        }
+        player.transform.position = newPos;
+        
+    }
+    
+}
